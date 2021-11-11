f42a123b9e8461ab86e8b35b0333e2e9800e6d59
hfog2066 <hfog2000@yahoo.com>
2021-11-10 21:00:05 -0500

    Exercise18 while

---
+++ Exercise18.java
@@ -0,0 +1,18 @@
+public class Exercise18{
+
+    public static void main(String[] args){
+
+	int x = 10;
+
+	while (x < 20) {
+
+	    System.out.println("Value of X: "+x);
+	    x++;
+	    System.out.println("\n");
+
+	}
+
+    }
+
+
+}

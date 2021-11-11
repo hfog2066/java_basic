5941849cc9c4113e11457227273adec634445504
hfog2066 <hfog2000@yahoo.com>
2021-11-10 20:31:39 -0500

    Exercise16 if-else

---
+++ Exercise16.java
@@ -0,0 +1,18 @@
+public class Exercise16 {
+
+    public static void main(String[] args){
+
+	long a = 1;
+	long b = 2;
+
+	if (a == b){
+	    System.out.println("True");
+
+	}else {
+	    System.out.println("False");
+
+	}
+
+    }
+
+}
